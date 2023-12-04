31b2d45c7e62bc980726d4269f1c542a2e8815ea
mohamed amin khadhraoui <khadhraoui.amin.2022@gmail.com>
2023-12-04 23:15:54 +0100

    first commit

---
+++ skillUp-main/src/main/java/com/Projet/Projet/Services/CategorieService.java
@@ -0,0 +1,22 @@
+package com.Projet.Projet.Services;
+
+import com.Projet.Projet.Entities.Categorie;
+
+import java.util.List;
+import java.util.Map;
+
+public interface CategorieService {
+
+    Categorie addCategorie(Categorie categorie);
+    Categorie getCategorieById(Long categorieId);
+    List<Categorie> getAllCategories();
+    void deleteCategorie(Long categorieId);
+    Categorie updateCategorie(Categorie categorie);
+    Categorie addFormationToCategorie(Long formationId, Long categorieId);
+    Categorie getCategorieByNom(String nom);
+    Categorie getCategorieByFormations_Id(Long id);
+    int getCategoriesNb();
+    List<Categorie> getAllCategoriesContainsTrainings();
+    List<Map<String, Float>> getPercentageOfCoursesInEachCategory();
+
+}
---
+++ src/main/java/com/Projet/Projet/Entities/Avis.java
@@ -0,0 +1,24 @@
+package com.Projet.Projet.Entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+public class Avis {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String commentaire;
+    private int note;
+    private LocalDate date;
+    @ManyToOne
+    @JsonIgnore
+    private Formation formation;
+    @ManyToOne
+    private Client client;
+}
